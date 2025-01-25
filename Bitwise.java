3ee212b5781a7babbea2adef587aef055a8ef287
Pallavi-Battu <pallavibattu5@gmail.com>
2025-01-25 12:36:44 +0530

    Update Bitwise.java

--- Bitwise.java
+++ Bitwise.java
@@ -1,10 +1,10 @@
  public class Bitwise {
     public static void main(String[] args) {
-        int num = 8; // Binary: 1000;
+        int num = 8;
 
         System.out.println("Original: " + num);
-        System.out.println("Left Shift (num << 1): " + (num << 1)); // 16
-        System.out.println("Right Shift (num >> 1): " + (num >> 1)); // 4
+        System.out.println("Left Shift (num << 1): " + (num << 1)); 
+        System.out.println("Right Shift (num >> 1): " + (num >> 1));
     }
 } 
     
